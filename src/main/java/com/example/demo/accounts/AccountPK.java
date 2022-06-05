41ab8224e22951965c8df7be3a631c7f6d6dda8a
Mona Rostami <jsprovat1@gmail.com>
2022-06-05 22:49:13 +0200

    full accounts done

---
+++ src/main/java/com/example/demo/accounts/AccountPK.java
@@ -0,0 +1,39 @@
+
+package com.example.demo.accounts;
+
+import lombok.*;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+public class AccountPK implements Serializable
+{
+    private String IBAN;
+    private long accountId;
+
+    @Override
+    public boolean equals(Object o) {
+        if ( this == o ) {
+            return true;
+        }
+        if ( o == null || getClass() != o.getClass() ) {
+            return false;
+        }
+        AccountPK pk = (AccountPK) o;
+        return Objects.equals( IBAN, pk.IBAN ) &&
+                Objects.equals( accountId, pk.accountId );
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash( IBAN, accountId );
+    }
+
+}
+
